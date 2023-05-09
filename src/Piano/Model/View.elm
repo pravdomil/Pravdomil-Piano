7c46be3ec5c85410faba90c52ec39691a030dab6
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-09 17:30:48 +0200

    next

--- src/Piano/Model/View.elm
+++ src/Piano/Model/View.elm
@@ -22,8 +22,9 @@ view model =
 
 viewBody : Piano.Model.Model -> Element Piano.Msg.Msg
 viewBody model =
-    column [ width fill, height fill, spacing 16 ]
-        [ viewHeader model
+    column [ width fill, height fill ]
+        [ el [ height (px 8) ] none
+        , viewHeader model
         , case model.file of
             Ok b ->
                 Piano.File.View.viewFile b
@@ -35,50 +36,47 @@ viewBody model =
 
 viewHeader : Piano.Model.Model -> Element Piano.Msg.Msg
 viewHeader model =
-    column [ width fill, paddingXY 16 0, spacing 16 ]
-        [ el [] none
-        , row [ spacing 8, centerX ]
-            [ case model.file of
-                Ok b ->
-                    text (File.name b.file)
+    row [ spacing 8, centerX ]
+        [ case model.file of
+            Ok b ->
+                text (File.name b.file)
 
-                Err b ->
-                    case b of
-                        Piano.Model.NotLoaded ->
-                            none
+            Err b ->
+                case b of
+                    Piano.Model.NotLoaded ->
+                        none
 
-                        Piano.Model.DecodeError ->
-                            text "Cannot read MIDI file."
-            , case model.file of
-                Ok b ->
-                    row [ spacing 8 ]
-                        (b.midi.tracks
-                            |> (\( x, x2 ) -> x :: x2)
-                            |> List.indexedMap
-                                (\i _ ->
-                                    let
-                                        number : Piano.TrackNumber.TrackNumber
-                                        number =
-                                            Piano.TrackNumber.fromInt i
-                                    in
-                                    inputCheckbox
-                                        theme
-                                        []
-                                        { icon = inputCheckboxIcon theme
-                                        , label = text (String.fromInt (Piano.TrackNumber.toInt number + 1))
-                                        , checked = b.disabledTracks |> Dict.Any.member Piano.TrackNumber.toInt number |> not
-                                        , onChange = Piano.Msg.TrackToggleRequested number
-                                        }
-                                )
-                        )
+                    Piano.Model.DecodeError ->
+                        text "Cannot read MIDI file."
+        , case model.file of
+            Ok b ->
+                row [ spacing 8 ]
+                    (b.midi.tracks
+                        |> (\( x, x2 ) -> x :: x2)
+                        |> List.indexedMap
+                            (\i _ ->
+                                let
+                                    number : Piano.TrackNumber.TrackNumber
+                                    number =
+                                        Piano.TrackNumber.fromInt i
+                                in
+                                inputCheckbox
+                                    theme
+                                    []
+                                    { icon = inputCheckboxIcon theme
+                                    , label = text (String.fromInt (Piano.TrackNumber.toInt number + 1))
+                                    , checked = b.disabledTracks |> Dict.Any.member Piano.TrackNumber.toInt number |> not
+                                    , onChange = Piano.Msg.TrackToggleRequested number
+                                    }
+                            )
+                    )
 
-                Err _ ->
-                    none
-            , button theme
-                []
-                { label = text "Select MIDI File"
-                , active = False
-                , onPress = Just Piano.Msg.FileSelectRequested
-                }
-            ]
+            Err _ ->
+                none
+        , button theme
+            []
+            { label = text "Select MIDI File"
+            , active = False
+            , onPress = Just Piano.Msg.FileSelectRequested
+            }
         ]
