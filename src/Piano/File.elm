9c8ba42ad23fba4016719f3d0bad09cd20a0189a
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-09 15:43:41 +0200

    next

---
+++ src/Piano/File.elm
@@ -0,0 +1,13 @@
+module Piano.File exposing (..)
+
+import Dict.Any
+import File
+import Midi
+import Piano.TrackNumber
+
+
+type alias File =
+    { file : File.File
+    , midi : Midi.File
+    , disabledTracks : Dict.Any.Dict Piano.TrackNumber.TrackNumber ()
+    }
