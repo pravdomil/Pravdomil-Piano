7127df8b56474aab85429fd0ff46e54bccd50f5e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-09 17:44:23 +0200

    next

---
+++ resources/esp32/Pravdomil Piano.ino
@@ -0,0 +1,80 @@
+#include <Arduino.h>
+#include <BLEMidi.h>
+
+#define PLAY(a, b)                                                             \
+  if ((status[i] & a) != (value & a)) {                                        \
+    if (value & a)                                                             \
+      BLEMidiServer.noteOn(0, 64 - b, 127);                                    \
+    else                                                                       \
+      BLEMidiServer.noteOff(0, 64 - b, 127);                                   \
+  }
+
+auto clockPin = 2;
+auto latchPin = 4;
+auto dataPin = 5;
+
+auto chips = 8;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(15, OUTPUT);
+  digitalWrite(15, 1);
+
+  BLEMidiServer.begin("Pravdomil Piano");
+}
+
+void loop() {
+  digitalWrite(latchPin, 0);
+  digitalWrite(latchPin, 1);
+
+  uint8_t status[chips];
+  for (int i = 0; i < chips; i++) {
+    auto value = shiftIn2(dataPin, clockPin, LSBFIRST);
+
+    Serial.print(value & 8 ? 0 : 1);
+    Serial.print(value & 1 ? 0 : 1);
+    Serial.print(value & 4 ? 0 : 1);
+    Serial.print(value & 2 ? 0 : 1);
+    Serial.print(value & 64 ? 0 : 1);
+    Serial.print(value & 32 ? 0 : 1);
+    Serial.print(value & 128 ? 0 : 1);
+    Serial.print(value & 16 ? 0 : 1);
+
+    if (BLEMidiServer.isConnected()) {
+      PLAY(8, 0 + ((chips - 1 - i) * 8));
+      PLAY(1, 1 + ((chips - 1 - i) * 8));
+      PLAY(4, 2 + ((chips - 1 - i) * 8));
+      PLAY(2, 3 + ((chips - 1 - i) * 8));
+      PLAY(64, 4 + ((chips - 1 - i) * 8));
+      PLAY(32, 5 + ((chips - 1 - i) * 8));
+      PLAY(128, 6 + ((chips - 1 - i) * 8));
+      PLAY(16, 7 + ((chips - 1 - i) * 8));
+    }
+
+    status[i] = value;
+  }
+
+  Serial.println();
+  Serial.flush();
+
+  delay(10);
+}
+
+uint8_t shiftIn2(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
+  uint8_t value = 0;
+  uint8_t i;
+
+  for (i = 0; i < 8; ++i) {
+    if (bitOrder == LSBFIRST)
+      value |= digitalRead(dataPin) << i;
+    else
+      value |= digitalRead(dataPin) << (7 - i);
+
+    digitalWrite(clockPin, LOW);
+    digitalWrite(clockPin, HIGH);
+  }
+  return value;
+}
