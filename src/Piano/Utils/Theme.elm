2065ba9472eab60202f7667167e7e03fdd0cd129
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-05-09 17:23:17 +0200

    next

--- src/Piano/Utils/Theme.elm
+++ src/Piano/Utils/Theme.elm
@@ -1,14 +1,14 @@
 module Piano.Utils.Theme exposing (..)
 
 import Element.PravdomilUi.Theme
-import Element.PravdomilUi.Theme.Light
+import Element.PravdomilUi.Theme.Basic
 
 
-theme : Element.PravdomilUi.Theme.Theme msg {}
+theme : Element.PravdomilUi.Theme.Theme msg
 theme =
-    Element.PravdomilUi.Theme.Light.theme style
+    Element.PravdomilUi.Theme.Basic.theme style
 
 
-style : Element.PravdomilUi.Theme.Style {}
+style : Element.PravdomilUi.Theme.Style
 style =
-    Element.PravdomilUi.Theme.Light.style
+    Element.PravdomilUi.Theme.Basic.style
