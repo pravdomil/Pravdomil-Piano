64e58121611e337bafac6f4e83bcd41f3aaec068
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-05 23:58:16 +0100

    next

--- scoregen/src/Piano/Model.elm
+++ scoregen/src/Piano/Model.elm
@@ -1,10 +1,13 @@
 module Piano.Model exposing (..)
 
+import Dict.Any
+import Midi
 import Piano.File
 
 
 type alias Model =
     { file : Result FileError Piano.File.File
+    , activeNotes : Dict.Any.Dict Midi.Note ()
     }
 
 
