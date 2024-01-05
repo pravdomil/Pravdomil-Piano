92bd0a69dc374c0afa437dd0d3193952afbdf664
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-05 23:31:03 +0100

    next

---
+++ scoregen/src-external/WebMidi.elm
@@ -0,0 +1,21 @@
+port module WebMidi exposing (..)
+
+import JavaScript
+import Json.Decode
+import Json.Encode
+import Task
+
+
+init : Task.Task JavaScript.Error ()
+init =
+    JavaScript.run
+        """
+navigator.requestMIDIAccess().then(a => {
+  a.inputs.forEach(b => { b.onmidimessage = c => { scope.ports.webMidi.send(c) } })
+})
+"""
+        Json.Encode.null
+        (Json.Decode.succeed ())
+
+
+port webMidi : (Json.Decode.Value -> msg) -> Sub msg
