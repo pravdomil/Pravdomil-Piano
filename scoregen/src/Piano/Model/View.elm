96fbb0daf2ed0a1fccd826e5a6cd66b5cf663eb5
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-05 22:58:23 +0100

    next

--- scoregen/src/Piano/Model/View.elm
+++ scoregen/src/Piano/Model/View.elm
@@ -18,7 +18,7 @@ view : Piano.Model.Model -> Browser.Document Piano.Msg.Msg
 view model =
     Browser.Document
         "Pravdomil Piano Score Generator"
-        [ layout [ width fill, height (px 800), Element.Background.color lightGray ] (viewBody model)
+        [ layout [ width fill, height (px 800), Element.Font.size 16, Element.Background.color lightGray ] (viewBody model)
         ]
 
 
