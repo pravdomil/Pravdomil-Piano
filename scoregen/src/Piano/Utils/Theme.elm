502d6ac46c0c8ebebd95b864b9754740ec591385
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-05 22:36:24 +0100

    next

--- scoregen/src/Piano/Utils/Theme.elm
+++ scoregen/src/Piano/Utils/Theme.elm
@@ -1,14 +1,72 @@
 module Piano.Utils.Theme exposing (..)
 
-import Element.PravdomilUi.Theme
-import Element.PravdomilUi.Theme.Basic
+import Element exposing (..)
+import Element.Border
+import Element.Font
+import Element.Region
 
 
-theme : Element.PravdomilUi.Theme.Theme msg
-theme =
-    Element.PravdomilUi.Theme.Basic.theme style
+type alias EdgesXY =
+    { left : Int
+    , right : Int
+    , top : Int
+    , bottom : Int
+    }
 
 
-style : Element.PravdomilUi.Theme.Style
-style =
-    Element.PravdomilUi.Theme.Basic.style
+
+--
+
+
+blue =
+    rgb 0 0 1
+
+
+
+--
+
+
+page a =
+    Element.Font.family
+        [ Element.Font.typeface "system-ui"
+        , Element.Font.typeface "-apple-system"
+        , Element.Font.typeface "Segoe UI"
+        , Element.Font.typeface "Roboto"
+        , Element.Font.typeface "Helvetica Neue"
+        , Element.Font.typeface "Arial"
+        , Element.Font.typeface "Noto Sans"
+        , Element.Font.typeface "Liberation Sans"
+        , Element.Font.sansSerif
+        , Element.Font.typeface "Apple Color Emoji"
+        , Element.Font.typeface "Segoe UI Emoji"
+        , Element.Font.typeface "Segoe UI Symbol"
+        , Element.Font.typeface "Noto Color Emoji"
+        ]
+        :: Element.Font.size 16
+        :: a
+
+
+heading1 a =
+    Element.Region.heading 1 :: Element.Font.size 32 :: a
+
+
+heading2 a =
+    Element.Region.heading 2 :: Element.Font.size 28 :: a
+
+
+heading3 a =
+    Element.Region.heading 3 :: Element.Font.size 24 :: a
+
+
+link_ a =
+    Element.Font.color blue
+        :: Element.Border.rounded 4
+        :: focused
+            [ Element.Border.shadow
+                { color = fromRgb ((\x -> { x | alpha = 0.4 }) (toRgb blue))
+                , offset = ( 0, 0 )
+                , blur = 0
+                , size = 4
+                }
+            ]
+        :: a
