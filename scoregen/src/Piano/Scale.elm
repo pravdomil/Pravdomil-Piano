495f562c74c681935eb1bf9582ea5300ea9d7065
pravdomil <2387356+pravdomil@users.noreply.github.com>
2024-01-10 00:01:55 +0100

    next

--- scoregen/src/Piano/Scale.elm
+++ scoregen/src/Piano/Scale.elm
@@ -17,3 +17,49 @@ type Scale
     | Aflat
     | Dflat
     | Gflat
+
+
+fromInt : Int -> Maybe Scale
+fromInt a =
+    case a of
+        0 ->
+            Just C
+
+        1 ->
+            Just G
+
+        2 ->
+            Just D
+
+        3 ->
+            Just A
+
+        4 ->
+            Just E
+
+        5 ->
+            Just B
+
+        6 ->
+            Just Fsharp
+
+        (-1) ->
+            Just F
+
+        (-2) ->
+            Just Bflat
+
+        (-3) ->
+            Just Eflat
+
+        (-4) ->
+            Just Aflat
+
+        (-5) ->
+            Just Dflat
+
+        (-6) ->
+            Just Gflat
+
+        _ ->
+            Nothing
